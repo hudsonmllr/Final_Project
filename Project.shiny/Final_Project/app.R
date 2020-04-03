271672a8d67aef4ae29caf7301458c118cab12a2
hudsonmllr <60484403+hudsonmllr@users.noreply.github.com>
2020-04-03 13:53:05 -0400

    Committing the shiny file and all additional data files.

---
+++ Project.shiny/Final_Project/app.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+ui <- navbarPage(
+    "Final Project Title",
+    tabPanel("Model",
+             fluidPage(
+                 titlePanel("Model Title"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(
+                             "plot_type",
+                             "Plot Type",
+                             c("Option A" = "a", "Option B" = "b")
+                         )),
+                     mainPanel(plotOutput("line_plot")))
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is where I talk about my project."),
+             h3("About Me"),
+             p("My name is ______ and I study ______. 
+             You can reach me at ______@college.harvard.edu.")))
